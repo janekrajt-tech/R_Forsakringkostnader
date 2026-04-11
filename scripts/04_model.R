a3cd59faa607f80a469696c90949909cf1c3be17
jan <janek.rajt@gmail.com>
2026-04-11 16:23:20 +0200

    rapport uppdattering och en ny modell

--- scripts/04_model.R
+++ scripts/04_model.R
@@ -1,2 +1,6 @@
 r_model <- lm(charges ~ age + bmi + smoker + chronic_condition + prior_claims, data = data_clean)
 print(summary(r_model))
+
+r_model_2 <- lm(charges ~ age + bmi + risk_score, data = data_clean)
+print(summary(r_model_2))
+
