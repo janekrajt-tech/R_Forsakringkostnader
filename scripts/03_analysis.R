02a26b95c7c8e90142c7658e096fea859c173ff6
jan <janek.rajt@gmail.com>
2026-04-16 15:40:48 +0200

    nya modeller + 'ndringar i rapporten

--- scripts/03_analysis.R
+++ scripts/03_analysis.R
@@ -3,38 +3,40 @@ p_customers_q <- ggplot(data_clean, aes(x = region)) +
   labs(
     title = "Antal kunder per region",
     x = "Region",
-    y = "Antal"
-  )
+    y = "Antal") + 
+  theme_minimal()
+
 p_agegroup_q <- ggplot(data_clean, aes(x = age_group)) +
   geom_bar() +
   labs(
     title = "Antal kunder per åldersgrupp",
     x = "Åldersgrupp",
-    y = "Antal"
-  )
+    y = "Antal")+ 
+  theme_minimal()
 
 p_charges_hist <- ggplot(data_clean, aes(x = charges)) +
   geom_histogram(binwidth = 1000) +
   labs(
     title = "Histogram över charges",
     x = "charges",
-    y = "Antal"
-  )
+    y = "Antal")+ 
+  theme_minimal()
+
 p_charges_vs_age <- ggplot(data_clean, aes(x = charges, y = age, colour = age_group)) + 
   geom_point() +
   labs(
     title = "Charges vs age",
     x = "charges",
-    y = "age"
-  )
+    y = "age") + 
+  theme_minimal()
 
 p_charges_bmi <- ggplot(data_clean, aes(x = charges, y = bmi)) + 
   geom_point() +
   labs(
     title = "Charges vs bmi",
     x = "Charges",
-    y = "Bmi"
-  )
+    y = "Bmi")+ 
+  theme_minimal()
 
 mean_charges_sex <- data_clean %>%
   group_by(sex) %>%
@@ -62,36 +64,38 @@ p_agegroup_charges_hist <- ggplot(data_clean, aes( x = age_group, y = charges)) 
   labs(
     title = "Boxplot Åldersgrupp vs Charges",
     x = "Åldersgrupp",
-    y = "Charges"
-  )
+    y = "Charges")+ 
+  theme_minimal()
 
 mean_charges_smoker <- data_clean %>%
   group_by(smoker) %>%
   summarise(mean_charges = mean(charges, na.rm = TRUE)) %>% 
   arrange(desc(mean_charges))
 
-smoker_charges_boxplot <- ggplot(data_clean, aes(x = smoker, y= charges)) +
+smoker_charges_boxplot <- ggplot(data_clean, aes(x = smoker, y= charges, fill = smoker)) +
   geom_boxplot() +
   labs(
     title = "Boxplot över charges för rökare och ickerökare",
     x = "Rökare",
-    y = "Charges"
-      )
+    y = "Charges") + 
+    theme_minimal()
+  
 
 p_charges_chronic <- ggplot(data_clean, aes(x = chronic_condition, y = charges)) +
   geom_boxplot() +
   labs(
     title = "Boxplot över kronisk sjukdom",
     x = "Kronisk Sjukdom",
-    y = "Charges"
-  )
+    y = "Charges")+ 
+  theme_minimal()
+
 p_prior_claims_charges <- ggplot(data_clean, aes(x = factor(prior_claims), y = charges))  +
   geom_boxplot() +
   labs(
     title = "Boxplot för tidigare claims", 
     x = "Antal tidigare claims",
-    y = "Charges"
-  )
+    y = "Charges")+ 
+  theme_minimal()
 
 ggsave(here("output", "figures", "p_customers_q.png"), p_customers_q)
 ggsave(here("output", "figures", "p_agegroup_q.png"), p_agegroup_q)
--- scripts/04_model.R
+++ scripts/04_model.R
@@ -1,6 +1,65 @@
-r_model <- lm(charges ~ age + bmi + smoker + chronic_condition + prior_claims, data = data_clean)
-print(summary(r_model))
+model_1 <- lm(charges ~ smoker + chronic_condition, data = data_clean)
+model_2 <- lm(charges ~ smoker + chronic_condition + prior_claims, data = data_clean)
+model_3 <- lm(charges ~ smoker + chronic_condition + prior_claims + age + bmi, data = data_clean)
 
-r_model_2 <- lm(charges ~ age + bmi + risk_score, data = data_clean)
-print(summary(r_model_2))
+summary(model_1)
+summary(model_3)
+
+
+
+
+model_comparison <- tibble(
+  model = c(
+    "Model 1: smoker + chronic_condition ",
+    "Model 2: + prior_claims",
+    "Model 3: + age + bmi"
+  ),
+  r_squared = c(
+    summary(model_1)$r.squared,
+    summary(model_2)$r.squared,
+    summary(model_3)$r.squared
+    
+  ),
+  adjusted_r_squared = c(
+    summary(model_1)$adj.r.squared,
+    summary(model_2)$adj.r.squared,
+    summary(model_3)$adj.r.squared
+
+  ),
+  residual_se = c(
+    summary(model_1)$sigma,
+    summary(model_2)$sigma,
+    summary(model_3)$sigma
+   
+  )
+)
+
+model_comparison
+
+model_3_diagnotics <- data_clean %>% 
+  mutate(
+    fitted_value = fitted(model_3),
+    residual = resid(model_3)
+  )
+
+
+act_res_diff <- model_3_diagnotics %>% 
+  select(charges, fitted_value, residual) %>% 
+  slice_head(n = 10)
+
+
+act_vs_pred <- ggplot(model_3_diagnotics, aes(x = fitted_value, y = charges)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red") +
+  labs(
+    title = "Faktiskt pris mot predikterat pris",
+    x = "Predikterat charges",
+    y = "Faktiskt charges"
+  ) +
+  theme_minimal()
+
+
+
+
+
 
